7cbc50b3b2fe19655e9d0de53b12bc9d3b058552
douglasmatosdev <douglasmatosdev@gmail.com>
2023-07-04 16:32:22 -0300

    fix center hero card when apply filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,11 @@ export default function Home() {
 }
 
 export const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-items: center;
   padding: 2rem;
 `
