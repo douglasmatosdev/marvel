3072ef5d65724216732b325d4868081ed44a8e93
douglasmatosdev <douglasmatosdev@gmail.com>
2023-07-04 16:30:14 -0300

    clean up and add use client

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { FC, useState, ChangeEvent, useCallback } from 'react'
+import React, { useState, ChangeEvent, useCallback } from 'react'
 import { SearchHeroContainer } from './styled'
 import { RiSearch2Line } from 'react-icons/ri'
 import ApiServices from '@/services/api'
--- src/services/secure.ts
+++ src/services/secure.ts
@@ -1,3 +1,4 @@
+'use client'
 /**
  * To start the application, you must first obtain the Marvel plublic key and
  * private key. To do this, go to https://developer.marvel.com/ create account,
