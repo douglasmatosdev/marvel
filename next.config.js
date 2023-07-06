6adeab64019462d811c376da11f3793301b21f72
douglasmatosdev <douglasmatosdev@gmail.com>
2023-07-06 17:26:34 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'i.annihil.us',
+                pathname: '/u/prod/marvel/i/mg/**',
+            },
+        ],
+    }
+}
 module.exports = nextConfig
